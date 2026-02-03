271caf0f0c5ebca2991b1d1800082effd35476a8
Eduastras <eduastras@outlook.com>
2026-02-03 17:46:58 -0300

    route middleware created

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,4 @@
+// Arquivo para fazer a junção das rotas.
 import { tickets } from "./tickets.js"
 
 export const routes = [...tickets]
--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 import http from "node:http"
 
 import { jsonHandler } from "./middlewares/jsonHandler.js"
+import { routeHandler } from "./middlewares/routeHandler.js"
 
 async function listener(request, response) {
   // Middleware do body da requisição.
   await jsonHandler(request, response)
-  console.log(request.body)
+  // Middleware de rotas.
+  routeHandler(request, response)
 }
 
 http.createServer(listener).listen(3333)
