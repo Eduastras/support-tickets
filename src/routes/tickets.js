37bc1ecec3ce5b08f8d7f01e31d253acda4202e7
Eduastras <eduastras@outlook.com>
2026-02-04 02:18:19 -0300

    showing all tickets

--- src/routes/tickets.js
+++ src/routes/tickets.js
@@ -1,4 +1,5 @@
 import { create } from "../controllers/tickets/create.js"
+import { index } from "../controllers/tickets/index.js"
 
 export const tickets = [
   {
@@ -7,5 +8,10 @@ export const tickets = [
     path: "/tickets",
     // Função que executa quando a rota for chamada.
     controller: create,
+  },
+  {
+    method: "GET",
+    path: "/tickets",
+    controller: index,
   },
 ]
